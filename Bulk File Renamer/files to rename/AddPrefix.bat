912b3aea3d67d318e967fe7c3ab3f7867b4d5151
Réda Pengam <71394086+RedaPengam@users.noreply.github.com>
2023-01-07 17:09:15 +0100

    Update AddPrefix.bat

--- Bulk File Renamer/files to rename/AddPrefix.bat
+++ Bulk File Renamer/files to rename/AddPrefix.bat
@@ -1,7 +1,7 @@
 :: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ::
 :: Batch script to rename files to ADD prefix					 ::
 :: Usage: AddPrefix.bat <file-extension> <prefix>				 ::
-:: example: RenZeroPad.bat ext IMG >> "file.ext" renamed to "IMGfile.ext"		 ::
+:: example: AddPrefix.bat ext IMG >> "file.ext" renamed to "IMGfile.ext"		 ::
 :: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ::
 
 @echo off
