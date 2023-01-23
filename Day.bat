554b1e858ec3c65dfdccca4c9fb66b898226e3ba
Réda Pengam <71394086+RedaPengam@users.noreply.github.com>
2023-01-23 20:27:40 +0100

    Update and rename Day.bat to Switch Windows Theme/day.bat

--- Day.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-Set "MyProcess=SystemSettings.exe"
-start /wait C:\Users\redap\AppData\Local\Microsoft\Windows\Themes\Day.theme
-tasklist | find /i "%MyProcess%">nul  && Taskkill /F /IM %MyProcess% || echo %MyProcess% Is not running
