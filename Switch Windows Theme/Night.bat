9a59c09fee680fed2f9e8b45f6227718c30ef2f1
Réda Pengam <71394086+RedaPengam@users.noreply.github.com>
2024-01-28 15:09:30 +0100

    Update and rename night.bat to Night.bat

--- Switch Windows Theme/Night.bat
+++ Switch Windows Theme/Night.bat
@@ -1,5 +1,6 @@
 @echo off
 Set "MyProcess=SystemSettings.exe"
 start /wait %localappdata%\Microsoft\Windows\Themes\Night.theme
+ping -n 3 127.0.0.1 > nul
 tasklist | find /i "%MyProcess%">nul  && Taskkill /F /IM %MyProcess% || echo %MyProcess% Is not running
 exit
